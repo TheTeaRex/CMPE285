5b7c2d3bdd5e47d80868b34000a05ebb1d5404c8
Alan Chan <achan@purestorage.com>
2017-12-06 00:22:03 -0800

    final_project: added css and error with invalid investments

--- final_project/app.py
+++ final_project/app.py
@@ -9,11 +9,26 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
+    errors = []
+    investments = ['e_invest', 'g_invest', 'i_invest', 'q_invest', 'v_invest']
     inputs = {}
     inputs['amount'] = float(request.form['invest_amt'])
+    inputs['e_invest'] = request.form.get('e_invest')
+    inputs['g_invest'] = request.form.get('g_invest')
+    inputs['i_invest'] = request.form.get('i_invest')
+    inputs['q_invest'] = request.form.get('q_invest')
+    inputs['v_invest'] = request.form.get('v_invest')
+    count = 0
+    for item in investments:
+        if inputs[item] is not None:
+            count += 1
+    if count == 0 or count > 2:
+        errors.append('Please select 1 or 2 investments strategies')
+
     return render_template('result.html',
-                            error='error message',
+                            errors=errors,
                             something='something')
+
 
 if __name__=='__main__':
     app.run()
