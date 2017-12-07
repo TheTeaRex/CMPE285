4c6e1d92ce08d1b82024d75af072f3b63ac5cc40
Alan Chan <achan@purestorage.com>
2017-12-07 01:36:11 -0800

    final_project: look back 30 days, have current, 5, 10 and 20 days old portfolio

--- final_project/app.py
+++ final_project/app.py
@@ -35,7 +35,7 @@ def process():
         'disney': {'name': 'Walt Disney Co.', 'ticker': 'dis'}
     }
     inputs = {}
-    inputs['amount'] = float(request.form['invest_amt'])
+    amount = float(request.form['invest_amt'])
     selected = {}
     count = 0
     for investment in investments:
@@ -55,6 +55,7 @@ def process():
         return render_template('result.html', errors=['Please make sure all selected investment strategies add up to 100%'])
 
     distribution = {}
+    # checking the stock distribution
     for item in selected:
         count = 0
         for stock in investments[item]:
@@ -66,19 +67,84 @@ def process():
             return render_template('result.html', errors=['Please make sure all stocks in an investment strategy add up to 100%'])
 
     data = {}
+    purchase_now = {}
+    purchase_5 = {}
+    purchase_10 = {}
+    total_5 = 0
+    total_10 = 0
+    purchase_20 = {}
+    total_20 = 0
     for stock in distribution:
-        data[translation[stock]['name']] = get_data.parse_data(
+        key = translation[stock]['name']
+        # getting the stock price
+        data[key] = get_data.parse_data(
             get_data.get_range(
                 translation[stock]['ticker'],
-                start=get_data.get_x_days_back(10),
+                start=get_data.get_x_days_back(30),
                 end=get_data.get_current_date()
             )
         )
 
-    print data
+        # calculate the dollar distribution if bought at this momnet
+        purchase_now[key] = {}
+        purchase_now[key]['distribution'] = distribution[stock]
+        purchase_now[key]['amt_dist'] = amount * distribution[stock] / 100
+        purchase_now[key]['share'] = int(purchase_now[key]['amt_dist'] / data[key][-1]['close'])
+
+        # calculate the dollar distribution if bought 5 business days ago
+        purchase_5[key] = {}
+        purchase_5[key]['distribution'] = distribution[stock]
+        purchase_5[key]['old_price'] = data[key][-5]['close']
+        purchase_5[key]['amt_dist'] = amount * distribution[stock] / 100
+        purchase_5[key]['share'] = int(purchase_5[key]['amt_dist'] / data[key][-5]['close'])
+        purchase_5[key]['cur_price'] = data[key][-1]['close']
+        purchase_5[key]['cur_value'] = data[key][-1]['close'] * purchase_5[key]['share']
+        total_5 += purchase_5[key]['cur_value']
+
+        # calculate the dollar distribution if bought 10 business days ago
+        purchase_10[key] = {}
+        purchase_10[key]['distribution'] = distribution[stock]
+        purchase_10[key]['old_price'] = data[key][-10]['close']
+        purchase_10[key]['amt_dist'] = amount * distribution[stock] / 100
+        purchase_10[key]['share'] = int(purchase_10[key]['amt_dist'] / data[key][-10]['close'])
+        purchase_10[key]['cur_price'] = data[key][-1]['close']
+        purchase_10[key]['cur_value'] = data[key][-1]['close'] * purchase_10[key]['share']
+        total_10 += purchase_10[key]['cur_value']
+
+        # calculate the dollar distribution if bought 20 business days ago
+        purchase_20[key] = {}
+        purchase_20[key]['distribution'] = distribution[stock]
+        purchase_20[key]['old_price'] = data[key][-20]['close']
+        purchase_20[key]['amt_dist'] = amount * distribution[stock] / 200
+        purchase_20[key]['share'] = int(purchase_20[key]['amt_dist'] / data[key][-20]['close'])
+        purchase_20[key]['cur_price'] = data[key][-1]['close']
+        purchase_20[key]['cur_value'] = data[key][-1]['close'] * purchase_20[key]['share']
+        total_20 += purchase_20[key]['cur_value']
+
+    profit_5 = total_5 - amount
+    percent_5 = profit_5 / amount
+    profit_10 = total_10 - amount
+    percent_10 = profit_10 / amount
+    profit_20 = total_20 - amount
+    percent_20 = profit_20 / amount
+
     return render_template('result.html',
                             errors=[],
-                            data=data)
+                            amount=amount,
+                            data=data,
+                            purchase_now=purchase_now,
+                            purchase_5=purchase_5,
+                            total_5=total_5,
+                            profit_5=profit_5,
+                            percent_5=percent_5,
+                            purchase_10=purchase_10,
+                            total_10=total_10,
+                            profit_10=profit_10,
+                            percent_10=percent_10,
+                            purchase_20=purchase_20,
+                            total_20=total_20,
+                            profit_20=profit_20,
+                            percent_20=percent_20)
 
 
 if __name__=='__main__':
